93f9621f660da2fe8b17cc023023964942944851
Patricia Garcia <uo282210@uniovi.es>
2024-04-06 19:56:02 +0200

    Refactor of tasks
    Instead of changing all the behavior of the tab component, added a function to transform Task[] into a list of TaskList. Context defines a Task[] and that list is transformed into a list of TaskList[] usable by the Tab component.

--- client/src/components/Context/TaskContext.tsx
+++ client/src/components/Context/TaskContext.tsx
@@ -1,49 +1,45 @@
-import { TaskList, Label } from "@/src/task/Scheme";
+import { TaskList, Label, taskToListOfTaskList } from "@/src/task/Scheme";
 import { ReactNode, createContext, useContext, useState } from "react";
 
 /**
  * Interface for the task context of the application.
  */
 export interface ITaskContext {
-    tabs: string[],
-    todo: TaskList[],
+    listNames: string[],
+    tasks: TaskList[],
     labels: Label[],
     selectedListIndex: number,
-    setTabs: React.Dispatch<React.SetStateAction<string[]>>,
-    setToDo: React.Dispatch<React.SetStateAction<TaskList[]>>,
+    setListNames: React.Dispatch<React.SetStateAction<string[]>>,
+    setTasks: React.Dispatch<React.SetStateAction<TaskList[]>>,
     setSelectedListIndex: React.Dispatch<React.SetStateAction<number>>,
 }
+
+const tasks = [
+    { done: false, title: "Disable button if new list name is empty string", listIndex: 0 },
+    { done: false, title: "Call granny", listIndex: 0 },
+    { done: false, title: "Buy vacuum", listIndex: 0 },
+    { done: false, title: "This is content 4", listIndex: 0 },
+    { done: false, title: "Call granny", listIndex: 0 },
+    { done: false, title: "Buy vacuum", listIndex: 0 },
+    { done: false, title: "Load dishwasher", listIndex: 1 },
+    { done: false, title: "Cleaning the bathroom", listIndex: 1 },
+    { done: false, title: "Do laundry", listIndex: 1 },
+    { done: false, title: "Mop", listIndex: 1 },
+    { done: false, title: "Call Giselle", listIndex: 2 },
+    { done: false, title: "Print EDP reports", listIndex: 2 },
+    { done: false, title: "Update documentation", listIndex: 2 },
+    { done: false, title: "Clean calendar", listIndex: 2 },
+]
 
 // TODO: replace it with solid logic
 // const with default context (it's not the one from the user's pod)
 const defaultContext: ITaskContext ={
-    tabs: ["TFG", "Home", "Work"],
-    todo: [
-        [
-            { done: false, title: "Disable button if new list name is empty string" },
-            { done: false, title: "Call granny" },
-            { done: false, title: "Buy vacuum" },
-            { done: false, title: "This is content 4" },
-            { done: false, title: "Call granny" },
-            { done: false, title: "Buy vacuum" },
-        ],
-        [
-            { done: false, title: "Load dishwasher" },
-            { done: false, title: "Cleaning the bathroom" },
-            { done: false, title: "Do laundry" },
-            { done: false, title: "Mop" },
-        ],
-        [
-            { done: false, title: "Call Giselle" },
-            { done: false, title: "Print EDP reports" },
-            { done: false, title: "Update documentation" },
-            { done: false, title: "Clean calendar" },
-        ],
-    ],
+    listNames: ["TFG", "Home", "Work"],
+    tasks: taskToListOfTaskList(tasks),
     labels: [{color: "#ad30ad", name: "school"}, {color: "#5930ab", name: "work"}, {color: "#e6a937", name: "home"}],
     selectedListIndex: 0,
-    setTabs: () => {},
-    setToDo: () => {},
+    setListNames: () => {},
+    setTasks: () => {},
     setSelectedListIndex: () => {},
 }
 // context with the default task context
@@ -67,11 +63,11 @@ export interface ITaskContextProvider {
  * @returns context provider
  */
 export function TaskProvider({children} : ITaskContextProvider) {
-    const [tabs, setTabs] = useState(defaultContext.tabs);
-    const [todo, setToDo] = useState(defaultContext.todo);
+    const [listNames, setListNames] = useState(defaultContext.listNames);
+    const [tasks, setTasks] = useState(defaultContext.tasks);
     const [selectedListIndex, setSelectedListIndex] = useState(defaultContext.selectedListIndex);
     const labels = defaultContext.labels;
-    const value ={tabs, todo, selectedListIndex, setTabs, setToDo, setSelectedListIndex, labels};
+    const value ={listNames, tasks, selectedListIndex, setListNames, setTasks, setSelectedListIndex, labels};
 
     return (
         <TaskContext.Provider value={value}>
