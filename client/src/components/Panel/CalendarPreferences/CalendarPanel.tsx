856ef1bdf24eab331d38ae88965e24cedcc30fe2
Patricia Garcia <uo282210@uniovi.es>
2024-04-13 21:34:12 +0200

    Week start day done

--- client/src/components/Panel/CalendarPreferences/CalendarPanel.tsx
+++ client/src/components/Panel/CalendarPreferences/CalendarPanel.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useEventContext } from "../../Context/EventContext";
+import toast from "react-hot-toast";
 
 
 
 export default function CalendarPanel() {
+    const {weekStart, setWeekStart} = useEventContext();
 
-    const [weekDays, setWeekDays] = useState(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]);
+    const [weekDays, setWeekDays] = useState([{dim: "M", day: "Monday"}, {dim: "Tue", day: "Tuesday"}, {dim: "W", day: "Wednesday"}, 
+        {dim: "Th", day: "Thursday"}, {dim: "F", day: "Friday"}, {dim: "Sat", day: "Saturday"}, {dim: "Sun", day: "Sunday"}]);
 
     return (
         <article className="calendar-settings">
             <h3>Calendar preferences</h3>
             <hr></hr>
             <section className="week-start">
-                <p>Starting day of week</p>
+                <p>Starting day of week (Monday by default):</p>
                 <div className="week-days-buttons">
                     {
                         weekDays.map((day, index) => {
-                            return (
-                                <button key={index}
+                             return (
+                                <button 
+                                    key={index} 
+                                    className={weekStart === (index + 1) ? "selected-day" : ""}
+                                    onClick={() => setWeekStart(index + 1)}
+                                    title={`Set to ${day.day}`}
                                 >
-                                    {day.toUpperCase().charAt(0)}
+                                    {day.dim}
                                 </button>
                             )
                         })
