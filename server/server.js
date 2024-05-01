03ea98b0df3f68e902f8ca78d2f7cb55a0a9ac82
Patricia Garcia <uo282210@uniovi.es>
2024-05-02 00:23:33 +0200

    trying to connect to the deployed server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 // ============== APPLICATION CONFIGURATIONS ==========
 const app = express();
-const PORT = 4000;
+const PORT = 80;
 const mongoUri = process.env.MONGO_URI;
 
 // ============== APPLICATION MODULES ================
